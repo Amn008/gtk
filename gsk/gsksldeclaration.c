2ef2ed4a36398b0a35adaf2765442fc4322376c8
Benjamin Otte <otte@redhat.com>
2017-10-17 01:02:44 +0200

    gsksl: Constant variables must be initialized
    If they aren't, throw a fit and just initialize them with 0.

--- gsk/gsksldeclaration.c
+++ gsk/gsksldeclaration.c
@@ -262,6 +262,13 @@ gsk_sl_declaration_parse_variable (GskSlScope           *scope,
     }
   gsk_sl_preprocessor_consume (preproc, NULL);
 
+  if (qualifier->storage == GSK_SL_STORAGE_GLOBAL_CONST &&
+      initial_value == NULL)
+    {
+      gsk_sl_preprocessor_error (preproc, DECLARATION, "Variables with \"const\" qualifier must be initialized with a constant value.");
+      initial_value = gsk_sl_value_new (type);
+    }
+
   variable = gsk_sl_declaration_new (&GSK_SL_DECLARATION_VARIABLE);
   variable->variable = gsk_sl_variable_new (name, type, qualifier, initial_value);
   variable->initial = initial;
